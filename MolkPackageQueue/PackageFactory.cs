c031886aacd7bb31d49a1c2defbace47e366498b
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2023-11-01 15:23:29 +0100

    CreatePackage in the PackageFactory Class now return a random priority. Payload now receive a random 10 char string as a packageName

--- MolkPackageQueue/PackageFactory.cs
+++ MolkPackageQueue/PackageFactory.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace MolkPackageQueue
 {
     class PackageFactory
     {
         Random randomizer = new Random();
-        public Package CreatePackage(Priority prio)
+        public Package CreatePackage()
         {
+            var randomPrio = (Priority)randomizer.Next(0, 3);
             //use randomizer to send in a prio-enum
-            return new Package(prio);
+            return new Package(randomPrio);
         }
     }
 }
